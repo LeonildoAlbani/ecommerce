91a2177c787ad4071bd7d7640edf62da172105c0
leonildo.albani <leonildo.albani@ciss.com.br>
2019-08-04 17:43:41 -0300

    Correção de configs para elastic search

--- build-dockers.sh
+++ build-dockers.sh
@@ -7,9 +7,10 @@ cd /Users/leonildo.albani/Documents/dev/pessoal/ecommerce/ecommerce-frontend
 docker build -f Dockerfile -t leonildoalbani/ecommerce-frontend .
 docker push leonildoalbani/ecommerce-frontend
 
-#cd /Users/leonildo.albani/Documents/dev/pessoal/ecommerce/ecommerce-backend
-#./mvnw clean package -DskipTests #esse skipTests não está aqui :)
-#docker push leonildoalbani/ecommerce-backend
+cd /Users/leonildo.albani/Documents/dev/pessoal/ecommerce/ecommerce-backend
+./mvnw clean package -DskipTests #esse skipTests não está aqui :)
+docker build -f src/main/docker/Dockerfile.jvm -t leonildoalbani/ecommerce-backend .
+docker push leonildoalbani/ecommerce-backend
 
 cd /Users/leonildo.albani/Documents/dev/pessoal/ecommerce
 
