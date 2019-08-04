74873221294225e035575d19a40287f62ac3d8d3
leonildo.albani <leonildo.albani@ciss.com.br>
2019-08-04 09:15:39 -0300

    Melhorando navbar

--- ecommerce-frontend/src/components/NavbarCarrinho/index.js
+++ ecommerce-frontend/src/components/NavbarCarrinho/index.js
@@ -5,24 +5,17 @@ import * as Actions from "../../store/actions";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
-import {FormControl} from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+import Icon from "../Icon";
 
-//Navbar exemplo retirada do react-bootstrap para teste -->
 const NavbarCarrinho = ({carrinho}) => (
     <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
-            </Form>
-        </Navbar.Collapse>
+        <Navbar.Brand>E-Commerce</Navbar.Brand>
+        <Nav className="mr-auto"/> {/* TODO wrap para mover para direita, se der tempo crio classe css */}
+        <Form inline>
+            {carrinho.length > 0 &&
+            <span>{carrinho.length} item(ns) adicionado(s)</span>}
+            <Icon icone="fa fa-shopping-cart"/>
+        </Form>
     </Navbar>
 );
 
